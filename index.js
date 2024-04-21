84ac38a372198373aa0311b1fe3b611b020f16e4
Sebille20 <alice.optina20@gmail.com>
2024-04-21 23:06:15 +0800

    deleted app.js, add index.js, put script start in package.json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World! Alice gwapa');
+    res.send('Hello World! Alice O.');
 });
 
 app.listen(port, () => {
