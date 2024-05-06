71e5a19c1a01b1e8acf5adfd7ea9a3fd81827c03
ALICEPC\Alice <alice.optina20@gmail.com>
2024-05-07 00:03:44 +0800

    change data

--- index.js
+++ index.js
@@ -13,33 +13,40 @@ app.use(cors());
 
 let resumeData = {
   personalInfo: {
-    name: "Ma. Alice Gay O. Optina",
+    firstname: "Ma. Alice Gay ",
+    lastname: "Optina",
     birthDate: "June 21",
     contactNo: "09951405048",
     address: "Mabolo Cebu City",
     objective: "I am an enthusiastic and dedicated 2nd-year BSIT student with a commitment to pursuing excellence in the field of Information Technology.",
     email: "alice.optina20@gmail.com",
     personalImg: "alice2.jpg",
-    objectiveIcon: "https://cdn4.iconfinder.com/data/icons/eldorado-user/40/user-128.png",
-    phoneIcon: "http://icons.iconarchive.com/icons/icons8/android/512/Mobile-Phone-2-icon.png",
-    emailIcon: "http://static.wixstatic.com/media/d822a6_af29afe70e1a4e9891f94a627914b6ec.png",
-    homeIcon: "https://icons.iconarchive.com/icons/rafiqul-hassan/blogger/512/Home-icon.png",
+    markerIcon: "https://i.postimg.cc/VL1v2bLL/icon-marker.png",
+    phoneIcon: "https://i.postimg.cc/fRC98fcN/icon-phone.png",
+    emailIcon: "https://i.postimg.cc/g0GxRXQT/icon-email.png",
   },
   skills: [
-    { description: "Web Development", expertiseLevel: "Advanced" },
-    { description: "Data Analysis", expertiseLevel: "Intermediate" },
-  ],
-  workExperience: [
-    { companyName: "Wa El Company", designation: "Tig hakot tubig", details: "Developed web applications", year: "2018-2020" },
+    { description: "Teamwork" },
+    { description: "Collaboration"  },
+    { description: "Adaptability"  },
   ],
   education: [
-    { school: "University of Southern Philippines Foundation ", year: "Present", address: "Lahug Cebu City" },
+    { school: "University of Southern Philippines Foundation ", course: "Bachelor of Science in Information Technology", year: "Present", address: "Lahug Cebu City" },
     { school: "Immaculata High School", year: "2015", address: "Baclayon Bohol" },
     { school: "Baclayon Central Elementary School", year: "2010-2011", address: "Baclayon Bohol" },
   ],
   personalReferences: [
     { name: "Jane", relationship: "Colleague", contactNumber: "0999999999" },
+    { name: "Kristine", relationship: "Colleague", contactNumber: "0999999999" },
   ],
+  language: [
+    { languagename: "Tagalog" },{ languagename: "English" }
+  ],
+
+  achievements: [
+    { name: "Dean's Lister", year: "First Year- First and Second Semester"},
+  ],
+
 };
 
 // Endpoint for personal information
